2cc1a7778b3d54d95fbcd73dcc0004098e662fd9
Conor Kelly <conorjkelly96@gmail.com>
2022-01-04 16:20:18 +0000

    renaming files

--- src/controllers/api/blog-controllers.js
+++ src/controllers/api/blog-controllers.js
@@ -2,7 +2,24 @@ const { logError } = require("../../helpers/utils");
 const { Blog, Comment } = require("../../models");
 
 // READ all blog posts
-const getAllBlogs = async (req, res) => {};
+const getAllBlogs = async (req, res) => {
+  try {
+    const data = await Blog.findAll({
+      include: [
+        {
+          model: Comment,
+        },
+      ],
+    });
+
+    return res.json({ success: true, data });
+  } catch (error) {
+    logError("GET Blogs".error.message);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to send response" });
+  }
+};
 
 // READ blog post by ID
 const getBlogsById = async (req, res) => {};
