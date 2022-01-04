9b21a17be683a386c556dfadb289f15ba3662a54
Conor Kelly <conorjkelly96@gmail.com>
2022-01-04 11:33:29 +0000

    user seeds

--- db/seeds/user-seeds.js
+++ db/seeds/user-seeds.js
@@ -1,5 +1,20 @@
 const { User } = require("../../src/models");
 
+const userData = [
+  {
+    email: "frodobaggins@theshire.com",
+    password: "Golumn123!",
+  },
+  {
+    email: "codewarrior@gmail.com",
+    password: "Swiftisterrible123!",
+  },
+  {
+    email: "nodetillidie@hotmail.com",
+    password: "BackendDev123!",
+  },
+];
+
 const seedUsers = () => User.bulkCreate(userData);
 
 module.exports = seedUsers;
