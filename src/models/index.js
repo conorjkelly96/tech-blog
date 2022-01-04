cd32a17a44e546f72923ae131becdfd29212fca3
Conor Kelly <conorjkelly96@gmail.com>
2022-01-04 11:03:18 +0000

    pseudo relationships added

--- src/models/index.js
+++ src/models/index.js
@@ -4,16 +4,22 @@ const Blog = require("./Blog");
 const Comment = require("./Comment");
 
 // Blog belongsTo User
+Blog.belongsTo(User);
 
 // User hasMany Blog
+User.hasMany(Blog);
 
 // Comment belongsTo Blog
+Comment.belongsTo(Blog);
 
 // Blog hasMany Comment
+Blog.hasMany(Comment);
 
 // Comment belongsTo User
+Comment.belongsTo(User);
 
 // User hasMany Comment
+User.hasMany(Comment);
 
 module.exports = {
   User,
