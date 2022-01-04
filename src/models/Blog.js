7ae0a2c97f0fe45781810b407afbe4a7caf18e4e
Conor Kelly <conorjkelly96@gmail.com>
2022-01-04 11:00:43 +0000

    blog model added

--- src/models/Blog.js
+++ src/models/Blog.js
@@ -1,8 +1,38 @@
 const { Model, DataTypes } = require("sequelize");
 
-const connection = require("../config/connection.js");
-const hashPassword = require("../hooks/hooks");
+const connection = require("../config/connection");
 
 class Blog extends Model {}
+
+Blog.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    connection,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: false,
+    modelName: "blog",
+  }
+);
 
 module.exports = Blog;
