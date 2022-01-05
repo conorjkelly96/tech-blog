4acd615c16aabbd2ff5cabd95b6aac45aa86ab84
Conor Kelly <conorjkelly96@gmail.com>
2022-01-05 13:54:34 +0000

    create associations

--- src/models/index.js
+++ src/models/index.js
@@ -4,22 +4,34 @@ const Blog = require("./Blog");
 const Comment = require("./Comment");
 
 // Blog belongsTo User
-Blog.belongsTo(User);
+Blog.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
 // User hasMany Blog
-User.hasMany(Blog);
+User.hasMany(Blog, {
+  foreignKey: "user_id",
+});
 
 // Comment belongsTo Blog
-Comment.belongsTo(Blog);
+Comment.belongsTo(Blog, {
+  foreignKey: "blog_id",
+});
 
 // Blog hasMany Comment
-Blog.hasMany(Comment);
+Blog.hasMany(Comment, {
+  foreignKey: "blog_id",
+});
 
 // Comment belongsTo User
-Comment.belongsTo(User);
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
 // User hasMany Comment
-User.hasMany(Comment);
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
 
 module.exports = {
   User,
