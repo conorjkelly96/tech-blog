ae4a367c32d9fcbdff00aede1c7d1ea4a5ee484f
Conor Kelly <conorjkelly96@gmail.com>
2022-01-06 10:06:14 +0000

    updated user model with username (oops!)

--- db/seeds/user-seeds.js
+++ db/seeds/user-seeds.js
@@ -4,14 +4,17 @@ const userData = [
   {
     email: "frodobaggins@theshire.com",
     password: "Golumn123!",
+    username: "LittleHobbit123",
   },
   {
     email: "codewarrior@gmail.com",
     password: "Swiftisterrible123!",
+    username: "PythonSucks_666",
   },
   {
     email: "nodetillidie@hotmail.com",
     password: "BackendDev123!",
+    username: "CameHereForTheFries",
   },
 ];
 
--- src/models/User.js
+++ src/models/User.js
@@ -19,6 +19,11 @@ const schema = {
       isEmail: true,
     },
   },
+  username: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+    unique: true,
+  },
   password: {
     type: DataTypes.STRING,
     allowNull: false,
