b8dcf7bbeb323970cfe2fd87c5bfe1b2d49caf0c
Conor Kelly <conorjkelly96@gmail.com>
2022-03-21 13:33:53 +0000

    delete blog function

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,28 @@
 const deleteBlogBtn = $("#delete-blog-btn");
 
+const makeRequest = async (url, method, body = {}) => {
+  const response = await fetch(url, {
+    method,
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  const data = await response.json();
+
+  console.log(data);
+
+  return data;
+};
+
 const handleDelete = async (event) => {
   const target = $(event.target);
   const blogId = target.attr("data-blog-id");
 
   try {
-    const data = await makeRequest(`/api/blogs/${blogId}`, "DELETE");
+    const data = await makeRequest(`/api/blog/${blogId}`, "DELETE");
+    console.log(data);
     if (data.success) {
       window.location.replace("/dashboard");
     } else {
