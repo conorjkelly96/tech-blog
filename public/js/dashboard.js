9d1ed877365bea78405ec98019579ac612f922c1
Conor Kelly <conorjkelly96@gmail.com>
2022-03-21 13:30:20 +0000

    dashbooard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,1 +1,19 @@
-console.log("connected");
+const deleteBlogBtn = $("#delete-blog-btn");
+
+const handleDelete = async (event) => {
+  const target = $(event.target);
+  const blogId = target.attr("data-blog-id");
+
+  try {
+    const data = await makeRequest(`/api/blogs/${blogId}`, "DELETE");
+    if (data.success) {
+      window.location.replace("/dashboard");
+    } else {
+      console.log("Failed to delete blog");
+    }
+  } catch (error) {
+    console.log("Failed to delete blog");
+  }
+};
+
+deleteBlogBtn.on("click", handleDelete);
